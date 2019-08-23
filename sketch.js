d1a537c50b2e34e32cf711446025a85385e65fc5
Mike <39208141+MikeG3@users.noreply.github.com>
2019-08-22 23:48:10 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -74,9 +74,9 @@ function setup() {
     hole.setRange(1, 0);
     //TEST SOUND
     soundWaves.play("G1", 0.1, 0, 1);
-    soundWaves.play("G2", 0.1, 1, 2);
-    soundWaves.play("G3", 0.1, 2, 3);
-    soundWaves.play("G4", 0.1, 3, 4);
+    soundWaves.play("G2", 0.1, 0, 2);
+    soundWaves.play("G3", 0.1, 0, 3);
+    soundWaves.play("G4", 0.1, 0, 4);
 }//close setup
 
 //DRAW LOOPS FOREVER
