8025b0e33d63424a51b2a179ceb5ac8f52f37c8c
Mike <39208141+MikeG3@users.noreply.github.com>
2019-08-29 13:08:08 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -120,7 +120,7 @@ function setup() {
    //INITIALIZE ALL SQUARES TO FLASE, NOT SELECTED
     for (i = 0 ; i < gridSizeY ; i++) {
        tones[i] =new p5.PolySynth();
-       tones[i].amp(0..5);
+       tones[i].amp(0.5);
        tones[i].freq(myNotes[i]);
        envs[i] = new P5.Env();
        envs[i].setADSR(0.001, 0.25, 0.1, 0.25);
