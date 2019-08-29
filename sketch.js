623f13d18b47c3ac166be5070fb253e8fca28cc1
Mike <39208141+MikeG3@users.noreply.github.com>
2019-08-29 14:12:07 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -122,8 +122,8 @@ function setup() {
 //        tones[i].amp(0.5);
 //        tones[i].freq(myNotes[i]);
 //        envs[i] = new P5.Env();
-       envs[i].setADSR(0.001, 0.25, 0.1, 0.25);
-       envs[i].envelope.setRange(1, 0);
+//        envs[i].setADSR(0.001, 0.25, 0.1, 0.25);
+//        envs[i].envelope.setRange(1, 0);
         //INITIALIZE ALL SQUARES TO FLASE, NOT SELECTED
         selectedSquares[i] = new Array(); 
         for (j = 0 ; j < gridSizeX ; j++) {
