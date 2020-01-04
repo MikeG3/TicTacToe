be0bf79c59f36126cd906b2d824fd216031b6a6f
Mike <39208141+MikeG3@users.noreply.github.com>
2020-01-04 02:14:54 -0500

    Update canvas.js

--- canvas.js
+++ canvas.js
@@ -14,82 +14,24 @@ var c = canvas.getContext('2d');
 var frameCounter = 0;
 var i = 0;
 var j = 0;
-var squareSize = 50;
-var aSquareR = 0;
-var aSquareG = 0;
-var aSquareB = 0;
-var aSquareA = 0.5;
-var bSquareR = 0;
-var bSquareG = 0;
-var bSquareB = 0;
-var incrementing = true;
-
-//strat drawing
-
+var k = 0;
+var grid = [ -1, -1, -1, -1, -1, -1, -1, -1, -1 ];
+var playerTurn = false;
 
 c.fillStyle = "#FF00EE";
 
-//CHECKERED BOARD
-// for (i = 0 ; i < windowHeight ; i+=squareSize ){
-//   for (j = 0 ; j < windowWidth ; j+=(2*squareSize) ) {  //j = (2*squareSize)
-//     if ( (j==0) && (i/40%2 != 0) ){     //if first in the row, and is an odd row number, offset by 1 square
-//       j += squareSize;
-//     }//close if
-//     c.fillRect(j, i, 40, 40);  
-//     document.write("i = " +i + "   ");
-//     document.write("j= " + j + "<br>");
-//   }//close inner for loop
-//   //update colors
-// }//close outter for loop
-// document.write("Window width = " + windowWidth + "<br>");
-// document.write("Window height = " + windowHeight + "<br>");
 
-function animate() {
+
+function playTicTacToe() {
   
   //variables 
 
+  //GAME LOOP
   
-  //UPDATE SQUARE SIZE
-  if (squareSize < 5) {
-    incrementing = true;
-  }
-    if (squareSize > 100 || squareSize > (windowHeight/2) || squareSize > (windowWidth/2)) {
-    incrementing = false;
-  }
-  if (incrementing){
-    squareSize++;
-  }
-    else {
-      squareSize--;
-    }
+  //DRAW GRID
   
-  //CHECKERED BOARD
-for (i = 0 ; i < windowHeight ; i+=squareSize ){
-  for (j = 0 ; j < windowWidth ; j+=(2*squareSize) ) {  //j = (2*squareSize)
-    if ( (j==0) && (i/squareSize%2 != 0) ){     //if first in the row, and is an odd row number, offset by 1 square
-      j += squareSize;
-    }//close if
-    //update colors
-    //aSquareR += 5;
-    aSquareR %= 255;
-    aSquareG += 25;
-    aSquareG %= 255;
-    aSquareB += 15;
-    aSquareB %= 255;
-    c.fillStyle = 'rgba(' + aSquareR + ',' + aSquareG + ',' + aSquareB + ','+ aSquareA + ')';
-    c.fillRect(j, i, squareSize, squareSize);  
-  }//close inner for loop
+  //PLACE NUMBERS
   
-  //update colors
-  //aSquareR += 5;
-  aSquareR %= 255;
-  aSquareG += 25;
-  aSquareG %= 255;
-  aSquareB += 15;
-  aSquareB %= 255;
-  c.fillStyle = 'rgba(' + aSquareR + ',' + aSquareG + ',' + aSquareB + ','+ aSquareA + ')';
-  
-}//close outter for loop
 // document.write("Window width = " + windowWidth + "<br>");
 // document.write("Window height = " + windowHeight + "<br>");
   
@@ -97,6 +39,6 @@ frameCounter++;
 requestAnimationFrame(animate);
 //output to console
 console.log(canvas);
-};
+};//close function play tic tac toe
 
-animate();
+playTicTacToe();
