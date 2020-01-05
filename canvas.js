10ac69076460ca69bf9fcb56a23abecc5668fccd
MikeG3 <harveymichaelgarner@hotmail.com>
2020-01-04 21:39:39 -0500

    update grid dimensions

--- canvas.js
+++ canvas.js
@@ -26,7 +26,7 @@ var x3 = x*3;
 var y1 = 1;
 var y2 = x;
 var y3 = x*2;
-var grid = [ -1, -1, -1, -1, -1, -1, -1, -1, -1 ];
+var grid = [ 1, 1, 1, 1, 1, 1, 1, 1, 1 ];
 var playerTurn = false;
 var xPos = 0;
 var yPos = 0;
@@ -60,34 +60,35 @@ function drawGrid(){
   else if (xPos == 1 && yPos == 2){ c.fillRect(x2, y3, x, y);}
   else if (xPos == 2 && yPos == 2){ c.fillRect(x3, y3, x, y);}
 
-  c.font = "30px Arial";
+  c.font = "50px Arial";
+  c.fillStyle = "#000000";
   //c.fillText(grid[0], x1, y1);
-  if (grid[0] == 0){  c.fillText("X", x1, y1); }//close if
-  else if (grid[0] == 1){ c.fillText("Y", x1, y1); }//close if
+  if (grid[0] == 0){  c.fillText("X", x1+x/2.5, y1+x/1.5); }//close if
+  else if (grid[0] == 1){ c.fillText("Y", x1+x/2.5, y1+x/1.5); }//close if
   //c.fillText(grid[1], x2, y1);
-  if (grid[1] == 0){ c.fillText("X", x2, y1); }//close if
-  else if (grid[1] == 1){ c.fillText("Y", x2, y1); }//close if
+  if (grid[1] == 0){ c.fillText("X", x2+x/2.5, y1+x/1.5); }//close if
+  else if (grid[1] == 1){ c.fillText("Y", x2+x/2.5, y1+x/1.5); }//close if
   //c.fillText(grid[2], x3, y1);
-  if (grid[2] == 0){ c.fillText("X", x3, y1); }//close if
-  else if (grid[2] == 1){ c.fillText("Y", x3, y1); }//close if
+  if (grid[2] == 0){ c.fillText("X", x3+x/2.5, y1+x/1.5); }//close if
+  else if (grid[2] == 1){ c.fillText("Y", x3+x/2.5, y1+x/1.5); }//close if
   //c.fillText(grid[3], x1, y2);
-  if (grid[3] == 0){ c.fillText("X", x1, y2); }//close if
-  else if (grid[3] == 1){ c.fillText("Y", x1, y2); }//close if
+  if (grid[3] == 0){ c.fillText("X", x1+x/2.5, y2+x/1.5); }//close if
+  else if (grid[3] == 1){ c.fillText("Y", x1+x/2.5, y2+x/1.5); }//close if
   //c.fillText(grid[4], x2, y2);
-  if (grid[4] == 0){ c.fillText("X", x2, y2); }//close if
-  else if (grid[4] == 1){ c.fillText("Y", x2, y2); }//close if
+  if (grid[4] == 0){ c.fillText("X", x2+x/2.5, y2+x/1.5); }//close if
+  else if (grid[4] == 1){ c.fillText("Y", x2+x/2.5, y2+x/1.5); }//close if
   //c.fillText(grid[5], x3, y2);
-  if (grid[5] == 0){ c.fillText("X", x3, y2); }//close if
-  else if (grid[5] == 1){ c.fillText("Y", x3, y2); }//close if
+  if (grid[5] == 0){ c.fillText("X", x3+x/2.5, y2+x/1.5); }//close if
+  else if (grid[5] == 1){ c.fillText("Y", x3+x/2.5, y2+x/1.5); }//close if
   //c.fillText(grid[6], x1, y3);
-  if (grid[6] == 0){ c.fillText("X", x1, y3); }//close if
-  else if (grid[6] == 1){ c.fillText("Y", x1, y3); }//close if
+  if (grid[6] == 0){ c.fillText("X", x1+x/2.5, y3+x/1.5); }//close if
+  else if (grid[6] == 1){ c.fillText("Y", x1+x/2.5, y3+x/1.5); }//close if
   //c.fillText(grid[7], x2, y3);
-  if (grid[7] == 0){ c.fillText("X", x2, y3); }//close if
-  else if (grid[7] == 1){ c.fillText("Y", x2, y3); }//close if
+  if (grid[7] == 0){ c.fillText("X", x2+x/2.5, y3+x/1.5); }//close if
+  else if (grid[7] == 1){ c.fillText("Y", x2+x/2.5, y3+x/1.5); }//close if
   //c.fillText(grid[8], x3, y3);
-  if (grid[8] == 0){ c.fillText("X", x3, y3); }//close if
-  else if (grid[8] == 1){ c.fillText("Y", x3, y3); }//close if
+  if (grid[8] == 0){ c.fillText("X", x3+x/2.5, y3+x/1.5); }//close if
+  else if (grid[8] == 1){ c.fillText("Y", x3+x/2.5, y3+x/1.5); }//close if
   c.stroke();
 }//close function draw grid
 
